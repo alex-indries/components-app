f9f1edb33c643c9d76f1b8fac88914aa94d684fb
alex-indries <alexandru.indries@verticaldigital.ca>
2022-07-07 11:27:44 +0300

    App is done!

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+  items: any = [
+    {
+      title: 'First item title',
+      content: 'First item content',
+    },
+    {
+      title: 'Second item title',
+      content: 'Second item content',
+    },
+    {
+      title: 'Third item title',
+      content: 'Third item content',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleModal() {
+    this.modalOpen = !this.modalOpen;
   }
-
 }
